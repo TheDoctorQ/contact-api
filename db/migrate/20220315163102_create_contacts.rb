dce4e0aaf7d92ebd275985a19230cf61c1e20c71
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-15 10:31:27 -0600

    fixes model

--- db/migrate/20220315163102_create_contacts.rb
+++ db/migrate/20220315163102_create_contacts.rb
@@ -1,6 +1,7 @@
-class CreateFirstNames < ActiveRecord::Migration[7.0]
+class CreateContacts < ActiveRecord::Migration[7.0]
   def change
-    create_table :first_names do |t|
+    create_table :contacts do |t|
+      t.string :first_name
       t.string :last_name
       t.string :email
       t.string :phone_number
